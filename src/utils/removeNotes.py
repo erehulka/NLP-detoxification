8fd1bb20b77f84be40f540a15b624e041f09689d
Erik Řehulka <erik.rehulka@bratislava.sk>
2024-04-28 15:06:55 +0200

    update

--- src/utils/removeNotes.py
+++ src/utils/removeNotes.py
@@ -11,6 +11,7 @@ input = pd.read_csv(sys.argv[1], sep='\t')
 for index, row in input.iterrows():
   processed = row['neutral_sentence']
   processed = re.sub(r'\(?Note:.*?$', '', processed)
+  processed = re.sub(r'\(?Translation:.*?$', '', processed)
   processed = re.sub(r'Output:', '', processed)
   input.at[index, 'neutral_sentence'] = processed
 
